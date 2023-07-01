5985429f87ac1394431cbcc719827502750dbb8e
Bruno Nabarrete <brunonabarrete@hotmail.com>
2023-07-01 18:25:47 -0300

    adiciono analutic

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import '@/styles/globals.css'
 import { Flowbite } from 'flowbite-react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return <Flowbite theme={{ dark: true }}>
     <Component {...pageProps} />
+    <Analytics/>
   </Flowbite>
 }
