badeece1f535b6a87d0f9d905b9d0d04b81ff46c
brunoNabarrete <brunonabarrete@hotmail.com>
2023-09-18 18:18:31 -0300

    adiciono o pixel do facebook

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Flowbite } from 'flowbite-react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Analytics } from '@vercel/analytics/react';
+import { useEffect } from 'react';
 
 const darkTheme = createTheme({
   palette: {
@@ -10,6 +11,15 @@ const darkTheme = createTheme({
 });
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('286018450834217');
+        ReactPixel.pageView();
+      });
+  }, []);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Flowbite theme={{ dark: true }}>
