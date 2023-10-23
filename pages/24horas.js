650ca9f5e48587e082ab254e6e04e8fda0473bd4
brunoNabarrete <brunonabarrete@hotmail.com>
2023-10-23 19:29:24 -0300

    adicionando um botão novo ao site, na parte da nova página

--- pages/24horas.js
+++ pages/24horas.js
@@ -3,11 +3,13 @@ import Footer from "./components/Footer"
 import WelcomeSection from "./components/WelcomeSection"
 import VideoChippix from "./components/videoChippix"
 import GameCards from "./components/GameCards"
+import StartButtonPlay from "./components/StartPlayButton"
 
 export default function HoursPage() {
   return (
     <div className="bg-gray-900">
     <Header />
+    <StartButtonPlay />
     <WelcomeSection />
     <VideoChippix/>
     <GameCards />
---
+++ pages/components/StartPlayButton/index.js
@@ -0,0 +1,16 @@
+export default function StartButtonPlay() {
+  return (
+    <div className="py-20 px-5 md:px-20 md:mb-12 bg-slate-800">
+      <h3 className="text-center w-full text-4xl text-white font-bold mb-8">
+        DEPÓSITO E DÚVIDAS 24 HORAS!
+      </h3>
+      <div className="flex items-center justify-center">
+
+      <a target='_blank' href="whatsapp://send?phone=5511988362072&text=Olá,%20vim%20pelo%20site%20para%20fazer%20parte%20do%20clube%20e%20cadastrar%20no%20chippix!!!" 
+      className="px-6 py-4 w-96 text-lg font-medium text-center text-white rounded-lg bg-green-true hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-true dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        WHATSAPP
+      </a>
+      </div>
+    </div>
+  )
+}
