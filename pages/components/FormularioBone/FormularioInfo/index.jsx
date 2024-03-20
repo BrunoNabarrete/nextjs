ee04064fba51a9939ca4199bc56e3c49cd3740a1
brunoNabarrete <brunonabarrete@hotmail.com>
2024-03-20 00:48:24 -0300

    mudando cors

--- pages/components/FormularioBone/FormularioInfo/index.jsx
+++ pages/components/FormularioBone/FormularioInfo/index.jsx
@@ -50,7 +50,6 @@ export default function Forminfo() {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify(data),
       });
